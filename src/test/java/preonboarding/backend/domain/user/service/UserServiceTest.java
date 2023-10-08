6719f0f990806890f7b7f07ea801fb1d7d1c220c
Siglee2247 <lgh941107@gmail.com>
2023-10-08 23:19:02 +0900

    [test]: 채용 지원 service 단위 테스트 구현
    Signed-off-by: Siglee2247 <lgh941107@gmail.com>

--- src/test/java/preonboarding/backend/domain/user/service/UserServiceTest.java
+++ src/test/java/preonboarding/backend/domain/user/service/UserServiceTest.java
@@ -1,6 +1,7 @@
 package preonboarding.backend.domain.user.service;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 
@@ -12,6 +13,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import preonboarding.backend.domain.recruit_announcement.entity.RecruitAnnouncement;
+import preonboarding.backend.domain.recruit_announcement.mock.RecruitAnnouncementMock;
+import preonboarding.backend.domain.recruit_announcement.service.RecruitAnnouncementService;
 import preonboarding.backend.domain.user.entity.User;
 import preonboarding.backend.domain.user.exception.UserExceptionCode;
 import preonboarding.backend.domain.user.repository.UserRepository;
@@ -24,7 +28,9 @@ class UserServiceTest {
     UserService service;
     @Mock
     UserRepository repository;
-
+    @Mock
+    RecruitAnnouncementService recruitAnnouncementService;
+    RecruitAnnouncementMock mock = new RecruitAnnouncementMock();
     @Test
     @DisplayName("사용자 등록 : 등록 성공")
     void post_user_test() throws Exception {
@@ -53,5 +59,23 @@ class UserServiceTest {
         // then
         Assertions.assertThatThrownBy(() -> service.postUser(postUser)).isInstanceOf(
                 BusinessLogicException.class).hasMessage(UserExceptionCode.USER_EXIST.getMessage());
+    }
+
+
+    @Test
+    @DisplayName("채용 공고 지원")
+    void recruit_test() throws Exception {
+        // given
+        User userMock = new User(1L, "김복자", "email", "password!!");
+
+        User postUser = new User(1L);
+        RecruitAnnouncement postAnnounce = RecruitAnnouncement.builder().id(1L).build();
+        given(repository.findById(anyLong())).willReturn(Optional.of(userMock));
+        given(recruitAnnouncementService.validAnnouncement(anyLong())).willReturn(
+                mock.getAfterRepoMock());
+        // when
+        User result = service.recruit(postUser, postAnnounce);
+        // then
+        Assertions.assertThat(result.getRecruitList()).isNotEmpty();
     }
 }
