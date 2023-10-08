95375faac81feec277623b2fadb0f1f90e055d49
Siglee2247 <lgh941107@gmail.com>
2023-10-09 01:46:41 +0900

    [feat]: RecruitAnnouncement 스웨거 등록 - AnnouncementPostRequestDto     - 예시 데이터 추가 - AnnouncementPatchRequestDto     - 예시 데이터 추가     - userId 필드 숨김 - 각 컨트롤러의 역할 기입

--- src/main/java/preonboarding/backend/domain/recruit_announcement/dto/AnnouncementPostRequestDto.java
+++ src/main/java/preonboarding/backend/domain/recruit_announcement/dto/AnnouncementPostRequestDto.java
@@ -1,5 +1,6 @@
 package preonboarding.backend.domain.recruit_announcement.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
@@ -16,17 +17,23 @@ public class AnnouncementPostRequestDto {
 
     @NotNull
     @Positive
+    @Schema(description = "회사 식별자",example = "1")
     private Long companyId;
     @NotNull
     @Length(max = 32)
+    @Schema(description = "근무 지역",example = "서울")
     private String workingArea;
     @NotNull
     @Positive
+    @Schema(description = "취업 보상금",example = "100000")
     private Integer compensationForEmployment;
     @NotNull
+    @Schema(description = "사용 기술 스택",example = "Java")
     private String skill;
     @NotNull
+    @Schema(description = "본문",example = "본문")
     private String content;
     @NotNull
+    @Schema(description = "채용 포지션",example = "백엔드 개발자")
     private String recruitPosition;
 }
