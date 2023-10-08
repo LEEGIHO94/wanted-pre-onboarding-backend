05fb9d9463db9f5010c7a44f8a4685f52ae79da7
Siglee2247 <lgh941107@gmail.com>
2023-10-08 23:05:13 +0900

    [feat]: 채용 공고 지원 컨트롤러 구현 - mapper 메서드 추가 - dto 구현     - UserRecruitPostRequestDto     - UserRecruitPostResponseDto
    Signed-off-by: Siglee2247 <lgh941107@gmail.com>

--- src/main/java/preonboarding/backend/domain/user/mapper/UserMapper.java
+++ src/main/java/preonboarding/backend/domain/user/mapper/UserMapper.java
@@ -1,11 +1,16 @@
 package preonboarding.backend.domain.user.mapper;
 
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import preonboarding.backend.domain.user.dto.UserIdResponseDto;
 import preonboarding.backend.domain.user.dto.UserPostRequestDto;
+import preonboarding.backend.domain.user.dto.UserRecruitPostResponseDto;
+import preonboarding.backend.domain.user.entity.Recruit;
 import preonboarding.backend.domain.user.entity.User;
+import preonboarding.backend.domain.user.exception.UserExceptionCode;
 import preonboarding.backend.dto.ResponseDto;
+import preonboarding.backend.exception.BusinessLogicException;
 
 @Component
 public class UserMapper {
@@ -21,7 +26,39 @@ public class UserMapper {
                 .build();
     }
 
+    public ResponseDto<UserRecruitPostResponseDto> toRecruitResponseDto(User result,
+            Long announceId, HttpStatus httpStatus) {
+        return ResponseDto.<UserRecruitPostResponseDto>builder()
+                .data(toRecruitResponseIdDto(result, announceId))
+                .status(httpStatus)
+                .build();
+    }
+
     private UserIdResponseDto toResponseIdDto(User user) {
         return new UserIdResponseDto(user.getId());
     }
+
+    private UserRecruitPostResponseDto toRecruitResponseIdDto(User user, Long announceId) {
+        Recruit recruit = byUser(user, announceId);
+        return new UserRecruitPostResponseDto(user.getId(),
+                recruit.getRecruitAnnouncement().getId(),
+                recruit.getRecruitAnnouncement().getCompany().getId(),
+                recruit.getRecruitAnnouncement().getCompany().getName());
+    }
+
+    private Recruit byUser(User user, Long announceId) {
+        List<Recruit> recruitList = user.getRecruitList();
+        for (Recruit recruit : recruitList) {
+            if (recruit.getRecruitAnnouncement().getId().equals(announceId)) {
+                return recruit;
+            }
+        }
+        throw new BusinessLogicException(UserExceptionCode.RECRUIT_NOT_FOUND);
+    }
+
+    public User toEntity(Long userId) {
+        return new User(userId);
+    }
+
+
 }
